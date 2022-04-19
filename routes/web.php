2d1c1532d5337e9e7e42711042ecd3ef8202e667
Thiago Albuquerque de Paula <thiago@tenil.com.br>
2022-04-19 15:37:56 -0300

    Criando o Menu

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 
 Route::get('/limpar-cache', function () {
     Artisan::call('cache:clear');
-    return "Cache is cleared";
+    return "Cache Limpo!";
 });
 
 require __DIR__.'/auth.php';
